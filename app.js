8b7f6aa3e1f8465f55219c5d8b8575af55ebf55d
Francis Li <mail@francisli.com>
2019-02-24 12:22:49 -0800

    Also map /dist to the build dir

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+app.use('/dist', express.static('build'))
 app.use('/', express.static('build'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
