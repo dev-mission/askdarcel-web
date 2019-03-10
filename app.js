2fb0c24c4107bf87a8b5dd0a0903d15aaec30254
Francis Li <mail@francisli.com>
2019-03-09 23:02:06 -0800

    Add a catchall for the SPA index page

--- app.js
+++ app.js
@@ -4,5 +4,8 @@ const port = process.env.PORT || 3000
 
 app.use('/dist', express.static('build'))
 app.use('/', express.static('build'))
+app.get('/*', (req, res) => {
+  res.sendFile(__dirname + '/build/index.html');
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
